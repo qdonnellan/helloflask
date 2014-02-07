592cffd67ad918236d5f2e3cf09c3b44aed3b025
qdonnellan <qdonnellan@gmail.com>
2014-02-06 21:56:08 -0600

    playing around with flask routes and templates … super simple

--- hello.py
+++ hello.py
@@ -1,11 +1,16 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return render_template('example.html', name='FooBar')
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 @app.route('/foo')
 def foo_bar():
